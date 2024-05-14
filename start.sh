e11185a8ef1bc6cefc9dfa6c0224207b5069df9d
donald <donaldford05091997@gmail.com>
2024-05-14 12:08:36 -0700

    UI and display text updates

--- start.sh
+++ start.sh
@@ -6,12 +6,4 @@ if [ -d "$HOME/.SuperSploit" ]; then
     sleep 0
   fi
   cd $HOME/.SuperSploit && python3 source/main.py
-else
-  if [ $UID == 0 ]; then
-    echo "Do not run as root"
-    exit
-  else
-    sleep 0
-  fi
-  cd /home/donald/.SuperSploit/ && python3 source/main.py
 fi
