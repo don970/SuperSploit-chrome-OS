f1c33810cd73d51313cd6347eddfe9618bd34d0f
donald <RecoveryEmail05091997@gmail.com>
2025-01-04 10:08:04 -0800

    setup fixes

--- executable.sh
+++ executable.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd $HOME
+mv SuperSploit-chrome-OS .SuperSploit
 echo "Making supersploit executable"
 printf '#include <stdio.h>\nint main(){\nsystem("bash $HOME/.SuperSploit/start.sh");}' > supersploit.c
 echo "Compiling executable"
@@ -9,7 +11,7 @@ echo "Making desktop file and app drawer icon"
 printf "[Desktop Entry]\nName=supersploit\nComment=Exploit Management framework\nExec=/bin/supersploit\nIcon=logo1.png\nTerminal=true\nType=Application\nCategories=Utility" > supersploit.desktop
 sudo cp supersploit.desktop /usr/share/applications/
 echo "Moving icon"
-sudo cp .data/.assets/logo1.png /usr/share/icons/locolor/16x16/apps/
+sudo cp $HOME/.SuperSploit/.data/.assets/logo1.png /usr/share/icons/locolor/16x16/apps/
 echo "Changing permissions for executable file"
 sudo chmod +x /bin/supersploit
 sudo rm supersploit.desktop supersploit.c
