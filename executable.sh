309e0b6a48e1a369098f2808fbcad80e71e97c6b
don970 <81848615+don970@users.noreply.github.com>
2025-01-19 14:35:31 -0800

    Update executable.sh

--- executable.sh
+++ executable.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cd $HOME
-mv SuperSploit-chrome-OS $HOME/.SuperSploit
 echo "Making supersploit executable"
 printf '#include <stdio.h>\nint main(){\nsystem("bash $HOME/.SuperSploit/start.sh");}' > supersploit.c
 echo "Compiling executable"
 gcc supersploit.c -o supersploit
 echo "Moving  supersploit to /bin folder"
-sudo mv supersploit /bin/supersploit
+sudo mv supersploit /bin/supersploit && rm supersploit.c
 echo "Making desktop file and app drawer icon"
 printf "[Desktop Entry]\nName=supersploit\nComment=Exploit Management framework\nExec=/bin/supersploit\nIcon=logo1.png\nTerminal=true\nType=Application\nCategories=Utility" > supersploit.desktop
 sudo cp supersploit.desktop /usr/share/applications/
